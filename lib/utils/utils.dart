7c4db4f52ad2f68ea6f2d95e5407bc2263c1b15a
Zaheer6110 <Zaheer6110@gmail.com>
2024-07-26 18:58:04 +0500

    Updated to latest

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -348,6 +348,8 @@ class Utils {
       Utils.isShowAdd = true;
       inititalize();
     }
+    Utils.isShowAdd = false;
+
   }
   static Future<void> inititalize() async {
     // CAS.setDebugMode(true);
