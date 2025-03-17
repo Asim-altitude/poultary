d07b59b684d545e0deeb58c9951022f74eb6e90a
Zaheer6110 <Zaheer6110@gmail.com>
2025-03-17 13:31:20 +0500

    Added Greek Language

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -546,7 +546,6 @@ class Utils {
       Utils.isShowAdd = true;
       inititalize();
     }
-    Utils.isShowAdd = false;
   }
   static Future<void> inititalize() async {
     // CAS.setDebugMode(true);
